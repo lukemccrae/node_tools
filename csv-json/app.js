8f8d493c938f818036c3fff6c20ff1fa0f6dee5b
“lukemccrae” <lukemccrae@gmail.com>
2021-02-27 16:07:22 -0800

    budget averages calculated

--- csv-json/app.js
+++ csv-json/app.js
@@ -17,20 +17,66 @@ function calculateAverages(filepath, cb) {
     });
 }
 
+function averageResult(resultTotal, num) {
+    let avg = resultTotal;
+    for (const [key, value] of Object.entries(resultTotal)) {
+        avg[key] = Math.round(value / num);
+    }
+    console.log(avg)
+    return avg;
+}
+
+function createNewBudgetAvgJson(filepath, jsObject) {
+    const jsonString = JSON.stringify(jsObject, null, 4);
+
+    fs.writeFile(filepath, jsonString, err => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("File written succesfully!")
+        }
+    })
+}
+
 
 //get the json
-calculateAverages('result.json', (err, data) => {
+calculateAverages('result.json', (err, months) => {
     if(err) {
         console.log(err);
     } else {
-        //check the new json
-        console.log(data)
-        console.log("hi")
+        let resultTotal = {}
 
-        //modify new json
-        // let newJson = modifyJson(data);
-        
-        //write newJson to the result.json file
-        // jsonWriter('result.json', newJson)
+        for (const month in months) {
+            //a single month in JSON
+            let jsonMonth = JSON.stringify(months[month]);
+
+            //month parsed
+            objMonth = JSON.parse(jsonMonth);
+
+            // console.log(`${month}: ${JSON.stringify(months[month])}`);
+            // console.log(" ")
+            // console.log(JSON.parse(jsonMonth))
+
+            //loop through categories of month
+            for (let i = 0; i < objMonth.length; i++) {
+                let currentEntry = objMonth[i];
+
+                //budget categories per month
+                for (const [key, value] of Object.entries(currentEntry)) {
+                    if(value == 0) continue;
+
+                    //result doesnt have the category, insert it
+                    if(!resultTotal[key]) {
+                        resultTotal[key] = value;
+
+                    //else, add the values
+                    } else {
+                        resultTotal[key] = (resultTotal[key] + value);
+                    }
+                  }
+            }
+
+          }
+          createNewBudgetAvgJson('BudgetAvg', averageResult(resultTotal, objMonth.length));
     }
 });
